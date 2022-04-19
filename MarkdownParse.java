5541fe541a30ab37a15686d86888b0976496fb35
Eric Sun <91984610+ericsun153@users.noreply.github.com>
2022-04-18 22:58:49 -0700

    Update code with test infinite loop and image

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,13 +11,19 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
-            int openBracket = markdown.indexOf("[", currentIndex);
-            int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+        while(currentIndex < markdown.length()-1) {
+            if(markdown.charAt(openBracket-1)=='!'){
+                int closeBracket = markdown.indexOf("]", openBracket);
+                int openParen = markdown.indexOf("(", closeBracket);
+                int closeParen = markdown.indexOf(")", openParen);
+                currentIndex = closeParen+1;
+            }else{
+                int closeBracket = markdown.indexOf("]", openBracket);
+                int openParen = markdown.indexOf("(", closeBracket);
+                int closeParen = markdown.indexOf(")", openParen);
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen+1;
+            }
         }
 
         return toReturn;
