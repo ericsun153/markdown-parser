d216733acb0d512d52d8d142fb15da1f98bac4af
Eric Sun <2397286134@qq.com>
2022-05-26 17:53:37 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -8,21 +8,21 @@ import java.util.List;
 
 public class MarkdownParseTest {
 
-    // @Test
-    // public void testSnippet1() throws IOException {
-    //     Path fileName = Path.of("Snippet1.md");
-    //     String content = Files.readString(fileName);
-    //     ArrayList<String> links = MarkdownParse.getLinks(content);
-    //     assertEquals(List.of("url.com","`google.com", "google.com", "ucsd.edu"), links);
-    // }
-
     @Test
-    public void testSnippet2() throws IOException {
-        Path fileName = Path.of("Snippet2.md");
+    public void testSnippet1() throws IOException {
+        Path fileName = Path.of("Snippet1.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(content);
-        assertEquals(List.of("a.com", "a.com(())", "example.com"), links);
+        assertEquals(List.of("`google.com", "google.com", "ucsd.edu"), links);
     }
+
+    // @Test
+    // public void testSnippet2() throws IOException {
+    //     Path fileName = Path.of("Snippet2.md");
+    //     String content = Files.readString(fileName);
+    //     ArrayList<String> links = MarkdownParse.getLinks(content);
+    //     assertEquals(List.of("a.com", "a.com(())", "example.com"), links);
+    // }
 
     // @Test
     // public void testSnippet3() throws IOException {
